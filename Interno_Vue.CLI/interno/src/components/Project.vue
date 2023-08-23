e6eca7614ab6ea9e6c7fc0d8abe51fa63a96a008
Aleksandra <alexnika@bk.ru>
2023-08-23 18:37:10 +0300

    modified task 6 in Interno_Vue.CLI/interno

--- Interno_Vue.CLI/interno/src/components/Project.vue
+++ Interno_Vue.CLI/interno/src/components/Project.vue
@@ -22,18 +22,16 @@
                 <div class="categories__content">
                     <button v-for="button in listOfButtons" :key="button.id" @click="changeProjectCategory(button.id)"
                         type="button"
-                        :class="['categories__btn', button.id === buttonIsActive ? 'categories__btn_active' : false]"> {{ button.title }}</button>
+                        :class="['categories__btn', button.id === buttonIsActive ? 'categories__btn_active' : false]"> {{
+                            button.title }}</button>
                 </div>
             </div>
             <!-- projects -->
             <div class="projects__items">
                 <div v-for="project in currentCategoryList" :key="project.id" class="projects__item">
                     <div class="star-box">
-                        <svg @click="changeStarColor"
-                        :class="['star', starIsActive ? 'star_active' : false]"
-                        :id="project.id"
-                            width="36"
-                            height="35" viewBox="0 0 36 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg @click="changeStarColor" class="star" :id="project.id" width="36" height="35"
+                            viewBox="0 0 36 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g filter="url(#filter0_d_993_156)">
                                 <path
                                     d="M16.0992 1.80951C16.7004 -0.0279021 19.2996 -0.0279021 19.9008 1.80951L21.7983 7.60879C22.0673 8.43084 22.8342 8.98685 23.6992 8.98685H29.8097C31.7506 8.98685 32.5539 11.4732 30.98 12.6088L26.0604 16.1581C25.3546 16.6674 25.0591 17.5748 25.3298 18.402L27.2146 24.1623C27.8166 26.0025 25.7137 27.539 24.1435 26.4062L19.1702 22.818C18.4715 22.3139 17.5285 22.3139 16.8298 22.818L11.8565 26.4062C10.2863 27.539 8.18335 26.0025 8.78545 24.1623L10.6702 18.402C10.9409 17.5748 10.6454 16.6674 9.93955 16.1581L5.02004 12.6088C3.44611 11.4732 4.24942 8.98685 6.19025 8.98685H12.3008C13.1658 8.98685 13.9327 8.43084 14.2017 7.60879L16.0992 1.80951Z"
@@ -236,7 +234,6 @@ export default {
                 }
             ],
             buttonIsActive: 'bedroom',
-            starIsActive: false,
         };
     },
     methods: {
@@ -252,9 +249,9 @@ export default {
         },
         changeStarColor(e) {
             const star = e.target.closest('.star');
-            this.allCategoriesList.forEach(project => {
+            this.allCategoriesList.forEach((project) => {
                 if (star.id === project.id) {
-                    return this.starIsActive = !this.starIsActive;
+                    star.classList.toggle('star_active');
                 }
             });
         },
