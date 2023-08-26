f29658527baf0e67c9c4b9c4a6cff2bedfd1fe9f
Aleksandra <alexnika@bk.ru>
2023-08-26 20:14:27 +0300

    added 7 task in Interno_Vue.CLI/interno

--- Interno_Vue.CLI/interno/src/App.vue
+++ Interno_Vue.CLI/interno/src/App.vue
@@ -1,31 +1,73 @@
 <template>
- <Header />
- <Footer />
- <HomePage />
- <BlogDetails />
- <Blog />
- <Project />
+  <Header />
+  <Footer />
+  <HomePage />
+  <BlogDetails />
+  <Blog />
+  <Project />
+  <ProjectDetails/>
+  <NotFound />
+  <!-- <Slider /> -->
+ 
+ </template>
+ 
+ <script>
+ import Header from './components/Header.vue'
+ import Footer from './components/Footer.vue'
+ import HomePage from './components/HomePage.vue'
+ import BlogDetails from './components/BlogDetails.vue'
+ import Blog from './components/Blog.vue'
+ import Project from './components/Project.vue'
+ import ProjectDetails from './components/ProjectDetails.vue'
+ import NotFound from './components/NotFound.vue'
+ import Slider from './components/Slider.vue'
+ 
+ export default {
+   name: 'App',
+   components: {
+     Header,
+     Footer,
+     HomePage,
+     BlogDetails,
+     Blog,
+     Project,
+     ProjectDetails,
+     NotFound,
+     Slider
+   }
+ }
+ </script>
+ <style lang="scss"></style>
+ 
 
+
+<!-- <template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import HomePage from './components/HomePage.vue'
-import BlogDetails from './components/BlogDetails.vue'
-import Blog from './components/Blog.vue'
-import Project from './components/Project.vue'
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Footer,
-    HomePage,
-    BlogDetails,
-    Blog,
-    Project
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
 }
-</script>
-<style lang="scss"></style>
+</style> -->
---
+++ Interno_Vue.CLI/interno/src/components/ProjectDetails.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="project-details__intro">
+        <div class="project-details__intro-content"></div>
+    </div>
+    <div class="project-details center">
+        <div v-for="info in projectInfo" class="project-details__content">
+            <h2 class="project-details__heading">{{ info.heading }}</h2>
+            <div class="project-details__text-box">
+                <p class="project-details__text">{{ info.firstparagraph }}</p>
+                <p class="project-details__text">{{ info.secondparagraph }}</p>
+            </div>
+            <Slider :slider-pictures="sliderPictures"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+import Slider from './Slider.vue'
+
+export default {
+    name: 'ProjectDetails',
+
+    components: {
+        Slider
+    },
+    computed: {
+        ...mapState(['projectInfo', 'sliderPictures']),
+    }
+
+
+}
+</script>
+
+<style></style>
+
+
